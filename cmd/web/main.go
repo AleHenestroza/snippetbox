bb8673cac6927e9a8dc7fc2b26560e09f2258a2b
Alejandro Henestroza <alehenestroza@gmail.com>
2022-11-02 22:11:22 -0300

    Chapter 3.2 - Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,9 +12,19 @@ func main() {
 	// of ":4000" and some short help text explaining what the flag controls.
 	// The value of the flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
-	// Use the flag.Parse() function *before* using the addr variable.
 	flag.Parse()
+
+	// Use log.New() to create a logger for writting information messages. This
+	// takes three parameters: the destination to write the logs to (os.Stdout),
+	// a string prefix for message (INFO followed by a tab), and flags to indicate
+	// what additional information to include (local date and time). Note that the
+	// flags are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way, but use stderr as
+	// the destination and use log.Lshortfile flag to include the relevant file name
+	// and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -33,7 +44,17 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set
+	// the ErrorLog field so that the server now uses the custom errorLog logger in
+	// the event of any problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
