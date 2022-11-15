25b6314a54707e21434ecef2cd7a2205f1e7e455
Alejandro Henestroza <alehenestroza@gmail.com>
2022-11-15 00:31:41 -0300

    Chapter 17 - Update Password (#8)
    * Display password change form and validate fields
    
    * Call PasswordUpdate in handler and display password change link in Account page
    
    * Implement PasswordUpdate for UserModel mock

--- internal/models/mocks/user.go
+++ internal/models/mocks/user.go
@@ -46,3 +46,13 @@ func (m *UserModel) Exists(id int) (bool, error) {
 		return false, nil
 	}
 }
+
+func (m *UserModel) PasswordUpdate(id int, currentPassowrd, newPassword string) error {
+	if id != 1 {
+		if currentPassowrd != "pa$$word" {
+			return models.ErrInvalidCredentials
+		}
+		return nil
+	}
+	return models.ErrNoRecord
+}
